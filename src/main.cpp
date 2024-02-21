dcf833242b618f0a56951f9757d93752de2346ee
Abhishek <abhi1489@uw.edu>
2024-02-21 12:37:33 -0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
+#include <Stepper.h>
+#include <Adafruit_NeoPixel.h>
+// How many internal neopixels do we have? some boards have more than one!
+#define NUMPIXELS        1
+#define PIN_NEOPIXEL     D3
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+
+// the setup routine runs once when you press reset:
+void setup() {
+  Serial.begin(115200);
+
+#if defined(NEOPIXEL_POWER)
+  // If this board has a power control pin, we must set it to output and high
+  // in order to enable the NeoPixels. We put this in an #if defined so it can
+  // be reused for other boards without compilation errors
+  pinMode(NEOPIXEL_POWER, OUTPUT);
+  digitalWrite(NEOPIXEL_POWER, HIGH);
+#endif
+
+  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  pixels.setBrightness(20); // not so bright
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // say hi
+  Serial.println("Hello!");
+  
+  // set color to red
+  pixels.fill(0xFF0000);
+  pixels.show();
+  delay(500); // wait half a second
+
+  // turn off
+  pixels.fill(0x000000);
+  pixels.show();
+  delay(500); // wait half a second
+}
