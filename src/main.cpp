6f55b043aa584ea8d4e71cd63f5c5804d031a51e
Abhishek <abhi1489@uw.edu>
2024-03-11 20:22:00 -0700

    Added Code changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,181 @@
-
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_ILI9341.h>
 #include <Stepper.h>
 #include <Adafruit_NeoPixel.h>
-// How many internal neopixels do we have? some boards have more than one!
-#define NUMPIXELS        1
-#define PIN_NEOPIXEL     D3
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+#include <Adafruit_I2CDevice.h>
+#include <Adafruit_SPIDevice.h>
+#include <Arduino.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
 
-// the setup routine runs once when you press reset:
+#define TFT_DC D7
+#define TFT_CS D6
+#define TFT_MOSI D10
+#define TFT_CLK D8
+#define TFT_RST D5
+#define TFT_MISO D9
+
+#define IN1 D1  // Motor driver input pins
+#define IN2 D2
+#define IN3 D3
+#define IN4 D4
+
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(4, D0, NEO_GRB + NEO_KHZ800);
+
+// Function prototypes
+class MyAdvertisedDeviceCallbacks;
+void notifyCallback(
+  BLERemoteCharacteristic* pBLERemoteCharacteristic,
+  uint8_t* pData,
+  size_t length,
+  bool isNotify);
+bool connectToServer();
+
+// BLE variables
+static BLEUUID serviceUUID("80ce2fe1-195b-437d-b811-9e0bc9dca6ed");
+static BLEUUID charUUID("41b23795-d294-432a-a5bd-5ddcdf444404");
+static boolean doConnect = false;
+static boolean connected = false;
+static boolean doScan = false;
+static BLERemoteCharacteristic* pRemoteCharacteristic;
+static BLEAdvertisedDevice* myDevice;
+
+const int stepsPerRevolution = 2048;
+Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4);
+
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    Serial.print("BLE Advertised Device found: ");
+    Serial.println(advertisedDevice.toString().c_str());
+
+    if (advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(serviceUUID)) {
+      BLEDevice::getScan()->stop();
+      myDevice = new BLEAdvertisedDevice(advertisedDevice);
+      doConnect = true;
+      doScan = true;
+    }
+  }
+};
+
 void setup() {
   Serial.begin(115200);
+  tft.begin();
+  tft.setRotation(3); // Rotate display 180 degrees
 
-#if defined(NEOPIXEL_POWER)
-  // If this board has a power control pin, we must set it to output and high
-  // in order to enable the NeoPixels. We put this in an #if defined so it can
-  // be reused for other boards without compilation errors
-  pinMode(NEOPIXEL_POWER, OUTPUT);
-  digitalWrite(NEOPIXEL_POWER, HIGH);
-#endif
-
-  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
-  pixels.setBrightness(20); // not so bright
+  BLEDevice::init("");
+  BLEScan* pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setInterval(1349);
+  pBLEScan->setWindow(449);
+  pBLEScan->setActiveScan(true);
+  pBLEScan->start(5, false);
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-  // say hi
-  Serial.println("Hello!");
-  
-  // set color to red
-  pixels.fill(0xFF0000);
-  pixels.show();
-  delay(500); // wait half a second
+  if (doConnect == true) {
+    if (connectToServer()) {
+      Serial.println("We are now connected to the BLE Server.");
+    } else {
+      Serial.println("We have failed to connect to the server; there is nothin more we will do.");
+    }
+    doConnect = false;
+  }
 
-  // turn off
-  pixels.fill(0x000000);
-  pixels.show();
-  delay(500); // wait half a second
+  if (connected) {
+    String newValue = "Time since boot: " + String(millis()/1000);
+    Serial.println("Setting new characteristic value to \"" + newValue + "\"");
+    pRemoteCharacteristic->writeValue(newValue.c_str(), newValue.length());
+  } else if(doScan){
+    BLEDevice::getScan()->start(0);
+  }
+
+  delay(1000);
+}
+
+class MyClientCallback : public BLEClientCallbacks {
+  void onConnect(BLEClient* pclient) {}
+  void onDisconnect(BLEClient* pclient) {
+    connected = false;
+    Serial.println("onDisconnect");
+  }
+};
+
+bool connectToServer() {
+  Serial.print("Forming a connection to ");
+  Serial.println(myDevice->getAddress().toString().c_str());
+
+  BLEClient* pClient = BLEDevice::createClient();
+  Serial.println(" - Created client");
+  pClient->setClientCallbacks(new MyClientCallback());
+  pClient->connect(myDevice);
+  Serial.println(" - Connected to server");
+  pClient->setMTU(517);
+
+  BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
+  if (pRemoteService == nullptr) {
+    Serial.print("Failed to find our service UUID: ");
+    Serial.println(serviceUUID.toString().c_str());
+    pClient->disconnect();
+    return false;
+  }
+  Serial.println(" - Found our service");
+
+  pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
+  if (pRemoteCharacteristic == nullptr) {
+    Serial.print("Failed to find our characteristic UUID: ");
+    Serial.println(charUUID.toString().c_str());
+    pClient->disconnect();
+    return false;
+  }
+  Serial.println(" - Found our characteristic");
+
+  if(pRemoteCharacteristic->canRead()) {
+    std::string value = pRemoteCharacteristic->readValue();
+    Serial.print("The characteristic value was: ");
+    Serial.println(value.c_str());
+  }
+
+  if(pRemoteCharacteristic->canNotify())
+    pRemoteCharacteristic->registerForNotify(notifyCallback);
+
+  connected = true;
+  return true;
+}
+
+void notifyCallback(
+  BLERemoteCharacteristic* pBLERemoteCharacteristic,
+  uint8_t* pData,
+  size_t length,
+  bool isNotify) {
+    Serial.print("Notify callback for characteristic ");
+    Serial.print(pBLERemoteCharacteristic->getUUID().toString().c_str());
+    Serial.print(" of data length ");
+    Serial.println(length);
+    Serial.print("data: ");
+    Serial.write(pData, length);
+    Serial.println();
+
+    tft.fillScreen(ILI9341_BLACK); // Clear screen
+    tft.setCursor(0, 0);
+    tft.setTextSize(2);
+    tft.setTextColor(ILI9341_WHITE);
+    tft.println("RGB Data:");
+
+    String rgbData = "";
+    for (int i = 0; i < length; i++) {
+      rgbData += (char)pData[i];
+      if (i < length - 1) rgbData += ",";
+    }
+    tft.println(rgbData);
+
+    // Draw circle with received RGB color
+    int center_x = tft.width() / 2;
+    int center_y = tft.height() / 2;
+    int radius = 50;
+    uint32_t color = tft.color565(pData[0], pData[1], pData[2]);
+    tft.fillCircle(center_x, center_y, radius, color);
 }
